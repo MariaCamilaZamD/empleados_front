3cc0a3eed0c311f8eb05bae168d4ea12bc402ecf
Camiari <camiari.zamd@gmail.com>
2022-11-14 18:24:04 -0500

    icono usuario y instalacion react-router-dom

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,10 @@
 import React from "react"; //imr
-import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Container, Nav, Dropdown, DropdownButton, Row} from "react-bootstrap";
+import './navbar.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
-export default class menu extends React.Component {
-  //cc
+export default class menu extends React.Component { //ccc
   constructor(props) {
     super(props);
     this.state = {};
@@ -10,28 +12,29 @@ export default class menu extends React.Component {
 
   render() {
     return (
-      <Navbar bg="primary" expand="lg" variant="dark">
+      <Navbar fixed="top" id="navbar" bg="primary" expand="lg" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Novios.com <span id = "usuario-sub-branm"></span></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              {/*  <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>*/}
             </Nav>
+            <DropdownButton id="dropdown-basic-button" title="Usuario">
+                <Dropdown.Header id= "dropdown-header">
+                  <Row>
+                    <FontAwesomeIcon icon={faUserCircle} />
+                  </Row>
+                  <Row>
+                    #Usuario#
+                  </Row>
+                </Dropdown.Header>
+                <Dropdown.Divider />
+              <Dropdown.Item href="#/action-1">Cerrar sesión</Dropdown.Item>
+              {/*<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>*/}
+            </DropdownButton>
           </Navbar.Collapse>
         </Container>
       </Navbar>
