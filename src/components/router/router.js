493780b208cc5eb2e8b801c849e3f849136aac39
Camiari <camiari.zamd@gmail.com>
2022-11-14 19:19:13 -0500

    creación ruta página de inicio

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../login/login'
+import Inicio from '../index/index';
+import Login from '../login/login';
 
 export default function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route exact path='/login' element={<Login/>} />
+                <Route exact path='/' element={<Inicio/>} />
                 <Route path='/*' element={(
                     <h1 style={{marginTop:300}}> 404 <br/> página no encontrada</h1>
                 )} />
